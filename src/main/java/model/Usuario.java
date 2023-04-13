9cd2b1818ad355fad6af17c5442a4b5383e56726
unknown <SUITE@SUITEA201-ST13.cibertec.edu.pe>
2023-04-12 21:13:23 -0500

    Proceso de login terminado

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,39 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Getter
+@Setter
+public class Usuario{
+	// atributos --> nombre de las columnas
+	@Id
+	private int cod_usua;
+	private String nom_usua;
+	private String ape_usua;
+	@Column(name = "usr_usua")
+	private String correo;
+	@Column(name = "cla_usua")
+	private String clave;
+	private String fna_usua;
+	private int idtipo;
+	private int est_usua;
+	
+	
+	//para la llave foranea FK
+	@ManyToOne
+	@JoinColumn(name = "idtipo", insertable= false, updatable=false)
+	private Tipo objTipo;
+	
+	
+	
+}
